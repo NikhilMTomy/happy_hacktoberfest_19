be9626bf8f653e0fb3a56bf1b91c64636afe023a
Greshma Shaji <43352861+ceccs17d24@users.noreply.github.com>
2020-10-01 01:03:30 +0530

    Split Array using slice()

---
+++ Smallerchunk.js
@@ -0,0 +1,14 @@
+function splitIntoChunk(arr, chunk) {
+
+    for (i=0; i < arr.length; i += chunk) {
+
+        let tempArray;
+        tempArray = arr.slice(i, i + chunk);
+        console.log(tempArray);
+    }
+
+}
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8];
+let chunk = 2;
+splitIntoChunk(array, chunk);
